efd47307d2dec169e0c110c03f0ccde1e3098abb
Chris Lewis <chrissam42@gmail.com>
2015-08-27 20:06:07 +0000

    added GPL notice at top of code

--- dev/2tdiff.php
+++ dev/2tdiff.php
@@ -1,5 +1,30 @@
-<?php
+<?php	
 namespace SecondThoughts;
+
+/*
+ * Copyright 2015 Chris Lewis <chris@chrislewis.codes>
+ *
+ * Takes the output of HTMLDiff and adds some information about certain
+ * proofreading-specific classes of differences: capitalization changes,
+ * punctuation changes, diacritic changes, word transpositions.
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * or see http://www.gnu.org/
+ *
+ */
 
 require_once('diff.php');
 require_once('htmldiff/html_diff.php');
