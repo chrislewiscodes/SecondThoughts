0270795ce36f4f8af52f46fe660c318fb667b0a4
Chris Lewis <chrissam42@gmail.com>
2015-09-19 19:47:03 +0000

    Mailchimp form fixes

--- generate.php
+++ generate.php
@@ -240,12 +240,8 @@ function section($section) {
 
 			<!-- MAILCHIMP -->
 			<form action="//secondthoughts.us11.list-manage.com/subscribe/post?u=e34c5ba197f6d60dce12b81b4&amp;id=a7ccdb3077" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required placeholder="Email address">
-				<div id="mce-responses">
-					<div class="response" id="mce-error-response" style="display:none"></div>
-					<div class="response" id="mce-success-response" style="display:none"></div>
-				</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 			    <div style="position: absolute; left: -5000px;"><input type="text" name="b_e34c5ba197f6d60dce12b81b4_a7ccdb3077" tabindex="-1" value=""></div>
+				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required placeholder="Email address">
 			    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
 			    </div>
 			</form>
