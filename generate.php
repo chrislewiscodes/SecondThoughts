70dd2c2df35c5e81951dbf6b01fe327de23213b5
Chris Lewis <chrissam42@gmail.com>
2015-09-22 03:51:21 +0000

    Spanish Subscribe

--- generate.php
+++ generate.php
@@ -27,6 +27,7 @@ $translations = array(
 	'participants' => 'participantes',
 	'more references' => 'más referencias',
 	'contact' => 'contacto',
+	'subscribe' => 'suscríbete',
 );
 
 function urlWithArgs($newargs) {
@@ -251,7 +252,7 @@ function section($section) {
 			<form action="//secondthoughts.us11.list-manage.com/subscribe/post?u=e34c5ba197f6d60dce12b81b4&amp;id=a7ccdb3077" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 			    <div style="position: absolute; left: -5000px;"><input type="text" name="b_e34c5ba197f6d60dce12b81b4_a7ccdb3077" tabindex="-1" value=""></div>
 				<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" required placeholder="Email">
-			    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+			    <input type="submit" value="<?php print translate('subscribe', 'ucfirst'); ?>" name="subscribe" id="mc-embedded-subscribe" class="button">
 			    </div>
 			</form>
 			<!-- /MAILCHIMP -->			
